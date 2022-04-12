8147030814818d4b6acbf7476f97cf8372216724
devnote-dev <inferno0745@gmail.com>
2022-04-12 18:23:46 +0100

    feat(application): implement cache

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,13 +1,31 @@
 import { application as app } from '../endpoints';
-import { AppServer, CreateUserOptions, PteroUser } from './structs';
+import { AppServer, CreateUserOptions, Node, PteroUser } from './structs';
 import http, { Auth } from '../http/rest';
 import transfomer from '../transformer';
 
+export interface AppCacheOptions {
+    cache?: boolean;
+}
+
 export class AppController {
     public auth: Auth;
+    public cache:{
+        allocations?: Map<number, object>;
+        nodes?: Map<number, Node>;
+        servers?: Map<number, AppServer>;
+        users?: Map<number, PteroUser>;
+    };
 
-    constructor(domain: string, key: string) {
+    constructor(domain: string, key: string, options: AppCacheOptions) {
         this.auth = { domain, key };
+        this.cache = {};
+
+        if (options.cache) {
+            this.cache.allocations = new Map<number, object>();
+            this.cache.nodes = new Map<number, Node>();
+            this.cache.servers = new Map<number, AppServer>();
+            this.cache.users = new Map<number, PteroUser>();
+        }
     }
 
     async getServers(id?: number): Promise<AppServer | AppServer[]> {
@@ -15,22 +33,36 @@ export class AppController {
             id ? app.servers.get(id) : app.servers.main(),
             this.auth
         );
-        return id
+        const res = id
             ? transfomer.fromAttributes<AppServer>(data.attributes)
             : transfomer.fromData<AppServer>(data.data);
+
+        if (this.cache.servers) {
+            Array.isArray(res)
+                ? res.forEach(s => this.cache.servers.set(s.id, s))
+                : this.cache.servers.set(res.id, res);
+        }
+        return res;
     }
 
-    async getUsers<T extends number | undefined>(
-        id?: T,
+    async getUsers(
+        id?: number,
         force: boolean = false
-    ): Promise<T extends undefined ? PteroUser[] : PteroUser> {
+    ): Promise<PteroUser[] | PteroUser> {
         const data = await http.get<PteroUser>(
             id ? app.users.get(id) : app.users.main(),
             this.auth
         );
-        return id
+        const res = id
             ? transfomer.fromAttributes<PteroUser>(data.attributes)
-            : transfomer.fromData<PteroUser>(data.data) as any;
+            : transfomer.fromData<PteroUser>(data.data);
+
+        if (this.cache.users) {
+            Array.isArray(res)
+                ? res.forEach(s => this.cache.users.set(s.id, s))
+                : this.cache.users.set(res.id, res);
+        }
+        return res;
     }
 
     async createUser(options: CreateUserOptions): Promise<PteroUser> {
@@ -42,8 +74,13 @@ export class AppController {
         return transfomer.fromAttributes(data.attributes);
     }
 
-    async updateUser(id: number, options: Partial<CreateUserOptions>): Promise<PteroUser> {
-        if (!Object.keys(options).length) throw new Error('Not enough options to update the user.');
+    async updateUser(
+        id: number,
+        options: Partial<CreateUserOptions>
+    ): Promise<PteroUser> {
+        if (!Object.keys(options).length)
+            throw new Error('Not enough options to update the user.');
+
         const user = await this.getUsers(id);
         const data = await http.patch<PteroUser>(
             app.users.get(id),
@@ -59,6 +96,10 @@ export class AppController {
     }
 }
 
-export function createApp(domain: string, auth: string) {
-    return new AppController(domain, auth);
+export function createApp(
+    domain: string,
+    auth: string,
+    options: AppCacheOptions = {}
+) {
+    return new AppController(domain, auth, options);
 }
