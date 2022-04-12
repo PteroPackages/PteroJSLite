c2c7fa8d97321975e19bf816c0766217bce17f52
devnote-dev <inferno0745@gmail.com>
2022-04-12 14:14:12 +0100

    feat(transfomer): add json transform func

--- src/transformer.ts
+++ src/transformer.ts
@@ -9,7 +9,7 @@ function fromAttributes<T>(data: { [key: string]: any }): T {
             !!v
         ) v = caseConv.toCamelCase(v);
         if (['created_at', 'updated_at'].includes(k) && v) v = Date.parse(v);
-        res[caseConv.camelCase(k)] = v || undefined;
+        res[caseConv.camelCase(k)] = v ?? undefined;
     }
     return res;
 }
@@ -18,7 +18,25 @@ function fromData<T>(data: { [key: string]: any }[]): T[] {
     return data.map<T>(d => fromAttributes(d.attributes));
 }
 
+function intoJSON(data: any): { [key: string]: any } {
+    let res: { [key: string]: any } = {};
+    for (let [k, v] of Object.entries(data)) {
+        if (
+            typeof v === 'object' &&
+            !Array.isArray(v) &&
+            !!v
+        ) v = caseConv.toSnakeCase(v);
+        if (
+            !['string', 'number', 'boolean'].includes(typeof v) &&
+            !!v
+        ) v = String(v);
+        res[caseConv.snakeCase(k)] = v ?? null;
+    }
+    return res;
+}
+
 export default {
     fromAttributes,
-    fromData
+    fromData,
+    intoJSON
 }
