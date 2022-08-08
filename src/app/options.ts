21bfda604ea5f42472bc2053e4fe633b0748019c
devnote-dev <inferno0745@gmail.com>
2022-08-08 16:53:48 +0100

    chore(app): separate options from types

---
+++ src/app/options.ts
@@ -0,0 +1,60 @@
+import { FeatureLimits, Limits } from '../common';
+
+export interface CreateServerOptions {
+    name:               string;
+    description?:       string;
+    externalId?:        string;
+    user:               number;
+    egg:                number;
+    dockerImage:        string;
+    startup:            string;
+    environment:        Record<string, string>;
+    skipScripts?:       boolean;
+    oomDisabled?:       boolean;
+    limits?:            Partial<Limits>;
+    featureLimits?:     Partial<FeatureLimits>;
+    allocation:{
+        default:        number;
+        additional?:    string[];
+    };
+    deploy?:{
+        locations:      number[];
+        portRange:      string[];
+        dedicatedIp:    string;
+    };
+    startOnCompletion?: boolean;
+}
+
+export interface CreateUserOptions {
+    email:      string;
+    username:   string;
+    firstName:  string;
+    lastName:   string;
+    password?:  string;
+    rootAdmin?: boolean;
+    externalId?: string;
+}
+
+export interface UpdateBuildOptions extends Partial<Limits & FeatureLimits> {
+    allocation?:        number;
+    oomDisabled?:       boolean;
+    addAllocations?:    number[];
+    removeAllocations?: number[];
+}
+
+export interface UpdateDetailsOptions {
+    name?:          string;
+    user?:          number;
+    externalId?:    string;
+    description?:   string;
+}
+
+export interface UpdateStartupOptions {
+    startup?:       string;
+    environment?:   Record<string, string>;
+    egg?:           number;
+    image?:         string;
+    skipScripts?:   boolean;
+}
+
+export interface UpdateUserOptions extends CreateUserOptions {}
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,7 +1,5 @@
 import { FeatureLimits, Limits } from '../common';
 
-// Structures
-
 export interface Allocation {
     id:         number;
     ip:         string;
@@ -72,67 +70,9 @@ export interface User {
     updatedAt:  number | null;
 }
 
-// Option interfaces/types
-
 export interface Container {
     startupCommand: number;
     image:          string;
     installed:      number;
     environment:    Record<string, string | number | boolean>;
-}
-
-export interface CreateServerOptions {
-    name:               string;
-    description?:       string;
-    externalId?:        string;
-    user:               number;
-    egg:                number;
-    dockerImage:        string;
-    startup:            string;
-    environment:        Record<string, string>;
-    skipScripts?:       boolean;
-    oomDisabled?:       boolean;
-    limits?:            Partial<Limits>;
-    featureLimits?:     Partial<FeatureLimits>;
-    allocation:{
-        default:        number;
-        additional?:    string[];
-    };
-    deploy?:{
-        locations:      number[];
-        portRange:      string[];
-        dedicatedIp:    string;
-    };
-    startOnCompletion?: boolean;
-}
-
-export interface CreateUserOptions {
-    email:      string;
-    username:   string;
-    firstName:  string;
-    lastName:   string;
-    password?:  string;
-    rootAdmin?: boolean;
-}
-
-export interface UpdateBuildOptions extends Partial<Limits & FeatureLimits> {
-    allocation?:        number;
-    oomDisabled?:       boolean;
-    addAllocations?:    number[];
-    removeAllocations?: number[];
-}
-
-export interface UpdateDetailsOptions {
-    name?:          string;
-    user?:          number;
-    externalId?:    string;
-    description?:   string;
-}
-
-export interface UpdateStartupOptions {
-    startup?:       string;
-    environment?:   Record<string, string>;
-    egg?:           number;
-    image?:         string;
-    skipScripts?:   boolean;
 }
