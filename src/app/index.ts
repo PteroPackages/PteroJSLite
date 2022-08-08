b0dedc1598098c22cffd9fe7e1156f025c78af87
devnote-dev <inferno0745@gmail.com>
2022-08-08 17:24:56 +0100

    feat(app): implement server methods

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,14 @@
 import { Auth, FractalData, FractalItem } from '../common';
-import { CreateUserOptions, UpdateUserOptions } from './options';
+import {
+    CreateServerOptions,
+    CreateUserOptions,
+    UpdateBuildOptions,
+    UpdateDetailsOptions,
+    UpdateStartupOptions,
+    UpdateUserOptions
+} from './options';
 import { application as routes } from '../routes';
-import { User } from './types';
+import { AppServer, User } from './types';
 import conv from '../conversions';
 import http from '../http';
 
@@ -23,7 +30,7 @@ class AppController {
                 default: throw new TypeError(`expected number or string; got ${typeof arg}`);
             }
 
-            const data = await http.get<FractalItem<User>>(path, this.auth)!;
+            const data = await http.get<FractalItem<User>>(path, this.auth);
             return conv.toCamelCase(data!.attributes);
         }
 
@@ -51,6 +58,89 @@ class AppController {
 
     async deleteUser(id: number): Promise<void> {
         return http.delete(routes.users.get(id), this.auth);
+    }
+
+    async getServers(): Promise<AppServer[]>;
+    async getServers(id: number | string): Promise<AppServer>;
+    async getServers(arg: any = null): Promise<any> {
+        let path = routes.servers.main();
+        if (arg) {
+            switch (typeof arg) {
+                case 'number': path = routes.servers.get(arg); break;
+                case 'string': path = routes.servers.ext(arg); break;
+                default: throw new TypeError(`expected number or string; got ${typeof arg}`);
+            }
+
+            const data = await http.get<FractalItem<AppServer>>(path, this.auth);
+            return conv.toCamelCase(data!.attributes, { pass:['environment'] });
+        }
+
+        const data = await http.get<FractalData<AppServer>>(path, this.auth);
+        return data!.data.map(
+            d => conv.toCamelCase(d.attributes, { pass:['environment'] })
+        );
+    }
+
+    async createServer(options: CreateServerOptions): Promise<AppServer> {
+        const data = await http.post<FractalItem<AppServer>>(
+            routes.servers.main(),
+            this.auth,
+            conv.toSnakeCase(options)
+        );
+        return conv.toCamelCase(data!.attributes);
+    }
+
+    async updateServerDetails(
+        id: number,
+        options: UpdateDetailsOptions
+    ): Promise<AppServer> {
+        const data = await http.patch<FractalItem<AppServer>>(
+            routes.servers.details(id),
+            this.auth,
+            conv.toSnakeCase(options)
+        );
+        return conv.toCamelCase(data!.attributes);
+    }
+
+    async updateServerBuild(
+        id: number,
+        options: UpdateBuildOptions
+    ): Promise<AppServer> {
+        const data = await http.patch<FractalItem<AppServer>>(
+            routes.servers.build(id),
+            this.auth,
+            conv.toSnakeCase(options)
+        );
+        return conv.toCamelCase(data!.attributes);
+    }
+
+    async updateServerStartup(
+        id: number,
+        options: UpdateStartupOptions
+    ): Promise<AppServer> {
+        const data = await http.patch<FractalItem<AppServer>>(
+            routes.servers.startup(id),
+            this.auth,
+            conv.toSnakeCase(options)
+        );
+        return conv.toCamelCase(data!.attributes);
+    }
+
+    async suspendServer(id: number): Promise<void> {
+        return http.post(routes.servers.suspend(id), this.auth);
+    }
+
+    async unsuspendServer(id: number): Promise<void> {
+        return http.post(routes.servers.unsuspend(id), this.auth);
+    }
+
+    async reinstallServer(id: number): Promise<void> {
+        return http.post(routes.servers.reinstall(id), this.auth);
+    }
+
+    async deleteServer(id: number, force: boolean = false): Promise<void> {
+        if (force) return http.delete(routes.servers.get(id) + '/force', this.auth);
+        return http.delete(routes.servers.get(id), this.auth);
     }
 }
 
