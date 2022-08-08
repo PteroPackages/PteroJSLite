6bf1389c3883742feb87cbb39ceb8a302af6231f
devnote-dev <inferno0745@gmail.com>
2022-08-08 16:39:21 +0100

    feat(routes): implement routes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import { version as v } from '../package.json';
 
 export const version = v;
 
+// Application API
+export { createApp } from './app';
+export * from './app/types';
+
 // HTTPS
 export { default as http } from './http';
 
 // General
 export * from './common';
 export * from './conversions';
+export { default as routes } from './routes';
