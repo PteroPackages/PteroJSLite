af85a47c9968b71d4bf0447c3c1c28d59d9fe1d3
devnote-dev <inferno0745@gmail.com>
2022-11-27 16:25:21 +0000

    chore: crlf -> lf & formatting

--- src/ws.ts
+++ src/ws.ts
@@ -1,5 +1,10 @@
 import { MessageEvent, WebSocket } from 'ws';
-import { PowerSignal, Resources, WebSocketAuth, WebSocketPayload } from './client/types';
+import {
+    PowerSignal,
+    Resources,
+    WebSocketAuth,
+    WebSocketPayload,
+} from './client/types';
 import { Auth } from './common';
 import conv from './conversions';
 import http from './http';
@@ -30,17 +35,25 @@ export interface IShard {
     setPowerState(signal: PowerSignal): void;
 }
 
-export type Shard = IShard & ThisType<{ auth: Auth, id: string, ws: WebSocket | null }>;
+export type Shard = IShard &
+    ThisType<{ auth: Auth; id: string; ws: WebSocket | null }>;
 
 export function createShard(id: string, auth: Auth): Shard {
     const impl = <IShard>{
         async connect() {
-            const auth = await http.get<{ data: WebSocketAuth }>(`/api/client/servers/${this.id}/websocket`, this.auth);
-            this.ws = new WebSocket(auth.data.socket, { origin: this.auth.url });
+            const auth = await http.get<{ data: WebSocketAuth }>(
+                `/api/client/servers/${this.id}/websocket`,
+                this.auth
+            );
+            this.ws = new WebSocket(auth.data.socket, {
+                origin: this.auth.url,
+            });
 
             this.ws.on('open', () => {
                 this.onDebug?.(`authenticating shard ${this.id}`);
-                this.ws.send(JSON.stringify({ event: 'auth', args:[auth.data.token] }));
+                this.ws.send(
+                    JSON.stringify({ event: 'auth', args: [auth.data.token] })
+                );
             });
 
             this.ws.on('message', (m: MessageEvent) => {
@@ -48,33 +61,74 @@ export function createShard(id: string, auth: Auth): Shard {
                 this.onRaw?.(packet);
 
                 switch (packet.event) {
-                    case 'auth success': this.onReady?.(); break;
-                    case 'token expiring': this._heartbeat(); break;
-                    case 'token expired': this.disconnect(); break;
-                    case 'backup completed': this.onBackupComplete?.(JSON.parse(packet.args.join())); break;
-                    case 'console output': this.onConsoleOutput?.(packet.args); break;
-                    case 'daemon message': this.onDaemonOutput?.(packet.args); break;
-                    case 'daemon error': this.onDaemonError?.(new Error(packet.args.join())); break;
-                    case 'install started': this.onInstallStart?.(); break;
-                    case 'install output': this.onInstallOutput?.(packet.args); break;
-                    case 'install completed': this.onInstallComplete?.(); break;
-                    case 'jwt error': this.onJWTError?.(new Error(packet.args.join())); break;
-                    case 'stats': this.onStatsUpdate?.(conv.toCamelCase(JSON.parse(packet.args.join()))); break;
-                    case 'status': this.onStatusUpdate?.(packet.args.join()); break;
-                    case 'transfer logs': this.onTransferOutput?.(packet.args); break;
-                    case 'transfer status': this.onTransferStatus?.(packet.args[0]); break;
-                    default: this.onError?.(`received unknown event '${packet.event}'`); break;
+                    case 'auth success':
+                        this.onReady?.();
+                        break;
+                    case 'token expiring':
+                        this._heartbeat();
+                        break;
+                    case 'token expired':
+                        this.disconnect();
+                        break;
+                    case 'backup completed':
+                        this.onBackupComplete?.(JSON.parse(packet.args.join()));
+                        break;
+                    case 'console output':
+                        this.onConsoleOutput?.(packet.args);
+                        break;
+                    case 'daemon message':
+                        this.onDaemonOutput?.(packet.args);
+                        break;
+                    case 'daemon error':
+                        this.onDaemonError?.(new Error(packet.args.join()));
+                        break;
+                    case 'install started':
+                        this.onInstallStart?.();
+                        break;
+                    case 'install output':
+                        this.onInstallOutput?.(packet.args);
+                        break;
+                    case 'install completed':
+                        this.onInstallComplete?.();
+                        break;
+                    case 'jwt error':
+                        this.onJWTError?.(new Error(packet.args.join()));
+                        break;
+                    case 'stats':
+                        this.onStatsUpdate?.(
+                            conv.toCamelCase(JSON.parse(packet.args.join()))
+                        );
+                        break;
+                    case 'status':
+                        this.onStatusUpdate?.(packet.args.join());
+                        break;
+                    case 'transfer logs':
+                        this.onTransferOutput?.(packet.args);
+                        break;
+                    case 'transfer status':
+                        this.onTransferStatus?.(packet.args[0]);
+                        break;
+                    default:
+                        this.onError?.(
+                            `received unknown event '${packet.event}'`
+                        );
+                        break;
                 }
             });
 
             this.ws.on('error', (e: Error) => this.onError?.(e));
-            this.ws.on('close', () => this.ws = null);
+            this.ws.on('close', () => (this.ws = null));
         },
 
         async _heartbeat() {
             this.onDebug?.(`reauthenticating shard ${this.id}`);
-            const auth = await http.get<{ data: WebSocketAuth }>(`/api/client/servers/${this.id}/websocket`, this.auth);
-            this.ws?.send(JSON.stringify({ event: 'auth', args:[auth.data.token] }));
+            const auth = await http.get<{ data: WebSocketAuth }>(
+                `/api/client/servers/${this.id}/websocket`,
+                this.auth
+            );
+            this.ws?.send(
+                JSON.stringify({ event: 'auth', args: [auth.data.token] })
+            );
         },
 
         destroy() {
@@ -83,19 +137,23 @@ export function createShard(id: string, auth: Auth): Shard {
         },
 
         requestLogs() {
-            this.ws?.send(JSON.stringify({ event: 'send logs', args:[] }));
+            this.ws?.send(JSON.stringify({ event: 'send logs', args: [] }));
         },
 
         requestStats() {
-            this.ws?.send(JSON.stringify({ event: 'send stats', args:[] }));
+            this.ws?.send(JSON.stringify({ event: 'send stats', args: [] }));
         },
 
         sendCommand(command) {
-            this.ws?.send(JSON.stringify({ event: 'send command', args:[command] }));
+            this.ws?.send(
+                JSON.stringify({ event: 'send command', args: [command] })
+            );
         },
 
         setPowerState(signal) {
-            this.ws?.send(JSON.stringify({ event: 'set state', args:[signal] }));
+            this.ws?.send(
+                JSON.stringify({ event: 'set state', args: [signal] })
+            );
         },
     };
 
@@ -103,6 +161,6 @@ export function createShard(id: string, auth: Auth): Shard {
         auth,
         id,
         ws: null,
-        ...impl
+        ...impl,
     };
 }
